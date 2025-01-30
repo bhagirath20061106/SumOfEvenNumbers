52d4c0a4f67173a4f052f9c771b0954a3200c5b3
pasupulabhagirathprabas <pbalu5077@gmail.com>
2025-01-30 09:41:41 +0530

    Add files via upload

---
+++ SumOfEvenNumbers.java
@@ -0,0 +1,16 @@
+package bhagirath;
+
+public class SumOfEvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int sum=0;
+int i=0;
+while(i<=100) {
+	sum+=i;
+	i+=2;
+}
+System.out.println("Sum of All Even Numbers Between 1 to 100: "+sum);
+	}
+
+}
